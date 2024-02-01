f3e5b7a8a5f54339db22e982f2bb5999b41ada5a
namoong0917 <namoong0917@gmail.com>
2024-02-02 02:49:49 +0900

    public 폴더 연결, html폴더 html파일들 연결

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
 app.set("port", process.env.PORT || 3000);
 app.get("/", (req, res) => {
   res.end("Hello World");
 });
+
+app.get("/test", (req, res) => {
+  res.end(
+    "<html><head><title>Test Title</title></head>\
+    <body><h1>Test</h1></body></html>"
+  );
+});
+
+app.get("/index", (req, res) => {
+  res.sendFile(__dirname + "/public/html/index.html");
+});
+
+app.get("/class", (req, res) => {
+  res.sendFile(__dirname + "/public/html/class.html");
+});
+
+app.get("/view", (req, res) => {
+  res.sendFile(__dirname + "/public/html/view.html");
+});
+
+app.get("/write", (req, res) => {
+  res.sendFile(__dirname + "/public/html/write.html");
+});
+
 app.listen(app.get("port"), () => {
   console.log("express server running on port 3000");
 });
